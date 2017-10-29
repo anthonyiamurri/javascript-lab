ed3712feabf65de8a399dd029409ddd05a9f6e3c
Anthony Iamurri <anthony.iamurri@gmail.com>
2017-10-29 18:21:53 -0400

    JS lab 4

--- game4.js
+++ game4.js
@@ -1,10 +1,9 @@
-// start game
 "use strict";
 
 var start = document.getElementById("start");
 var username = "";
 
-start.onclick = function() {
+start.onclick = function startGame() {
   document.getElementById("begin").style.display = "none";
   var username = prompt("Please enter your name.");
   user.name = username;
@@ -14,7 +13,7 @@ start.onclick = function() {
 var user = {
   name: "",
   health: 40,
-  healCount: 2,
+  healCount: 0,
   wins: 0,
   generateAttackDamage: function() {
     return Math.floor(Math.random() * 3) + 1;
@@ -32,70 +31,41 @@ var computer = {
   }
 };
 
-var startAttack = document.getElementById("attack");
-startAttack = function() {
+var startCombat = document.getElementById("attack");
+startCombat.onclick = function() {
   user.health -= computer.generateAttackDamage();
   computer.health -= user.generateAttackDamage();
-  document.getElementById("userscore").innerHTML = "You attacked! " + user.name + " has " + user.health + " health point(s) left.";
-  document.getElementById("computerscore").innerHTML = computer.name + " has " + computer.health + " health point(s) left.";
-};
+  document.getElementById("userscore").innerHTML = "You attacked! </p><p>" + user.name + " has " + user.health + " health point(s) left.";
+  document.getElementById("computerscore").textContent = computer.name + " has " + computer.health + " health point(s) left.";
+  document.getElementById("playerHealthStats").setAttribute("value", user.health);
+  document.getElementById("enemyStats").setAttribute("value", computer.health);
+  if (computer.health <= 0 && user.health > 0) {
+    user.wins++;
+    computer.health = 10;
+    document.getElementById("rounds").textContent = "You have won " + user.wins + " round(s).";
+    document.getElementById("playerWinCount").setAttribute("value", user.wins);
+  } else if (user.health <= 0) {
+      document.getElementById("gameplay").innerHTML = "Almighty Grant! beat you.";
+  } else if (user.wins == 3) {
+      document.getElementById("gameplay").innerHTML = "<h1>You won!</h1>";
+    }
+  };
 
 var startHeal = document.getElementById("heal");
 startHeal.onclick = function() {
   user.heal();
-  document.getElementById("userscore").innerHTML = "You healed. " + user.name + " has " + user.health + " health point(s).";
+  user.healCount++;
+  if (user.healCount <= 2) {
+    document.getElementById("userscore").innerHTML = "You healed. </p><p>" + user.name + " has " + user.health + " health point(s).";
+    document.getElementById("playerHealCount").setAttribute("value", user.healCount);
+} else if (user.healCount > 2) {
+    document.getElementById("heal").disabled = true;
+    document.getElementById("heal").style.backgroundColor = "gray";
+    document.getElementById("heals").innerHTML = "You're out of heals.";
+}
 };
 
 var quitGame = document.getElementById("quit");
 quitGame.onclick = function() {
   document.getElementById("gameplay").innerHTML = "That's a wrap.";
 };
-
-var startCombat = {
-  startAttack.onclick = startAttack();
-  startHeal.onclick = startHeal();
-  quitGame.onclick = quitGame();
-};
-
-startCombat();
-
-
-
-//
-//
-// startGame();
-//
-// // action
-//
-// function startCombat() {
-//
-//   while (computer.health > 0 && user.health > 0) {
-//     var combat = prompt("Would you like to attack, heal, or quit (a/h/q)?");
-//     if (combat.toLowerCase() === "a") {
-//       user.health -= computer.generateAttackDamage();
-//       computer.health -= user.generateAttackDamage();
-//       console.log(user.name + " has " + user.health + " health points.");
-//       console.log(computer.name + " has " + computer.health + " health points.");
-//       if (user.health <= 0) {
-//         console.log("You lost. Game over!");
-//       } else if (computer.health <= 0 && user.wins >= 4) {
-//         console.log("You beat Almighty Grant!");
-//         break;
-//       } else if (computer.health <= 0) {
-//         computer.health = 10;
-//         user.wins++;
-//         console.log("You have won " + user.wins + " round(s).")
-//       }
-//     } else if (combat.toLowerCase() === "q") {
-//       console.log("Goodbye!!");
-//       break;
-//     } else if (combat.toLowerCase() === "h") {
-//       if (user.healCount > 1) {
-//         user.heal();
-//         user.healCount--;
-//         computer.generateAttackDamage();
-//         console.log("You now have " + user.health + " health points.");
-//       }
-//     }
-//   }
-// }
